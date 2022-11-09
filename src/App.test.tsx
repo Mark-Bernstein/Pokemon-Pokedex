7805006eb9900e659d3d3d5fd8e45509ea93acdf
Sterling Watts <sterling.watts@mindera.com>
2022-11-09 15:42:37 -0800

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+it("says hello world", () => {
   render(<App />);
   const linkElement = screen.getByText(/hello world/i);
   expect(linkElement).toBeInTheDocument();
